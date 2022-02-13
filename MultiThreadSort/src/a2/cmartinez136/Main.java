9059a67fe9a5e1cfca3003e80290582c954f4c4a
Carolina Martinez <cmarinez136@csub.edu>
2022-02-12 16:58:18 -0800

    a2 Lab Done

--- MultiThreadSort/src/a2/cmartinez136/Main.java
+++ MultiThreadSort/src/a2/cmartinez136/Main.java
@@ -11,7 +11,7 @@ public class Main{
         Scanner scan = new Scanner(System.in);
         Random ran = new Random();
 
-        System.out.print("Do you want a [S]ingle Sort or a [D]ual sort? ");
+        System.out.print("Do you want a [S]ingle Sort, [D]ual sort, or [Q]uad sort?");
         char selection = scan.next().charAt(0);
 
         System.out.print("How many items do you want to sort?");
@@ -32,7 +32,43 @@ public class Main{
         switch (selection) {
             case 's', 'S' -> SingleSort(items);
             case 'd', 'D' -> DualSort(items);
+            case 'q', 'Q' -> QuadSort(items);
         }
+    }
+
+    public static void QuadSort(Item[] items) throws InterruptedException {
+        int mid = Math.round(items.length / 2f);
+        ThreadSort t1 = new ThreadSort(items, 0, mid);
+        ThreadSort t2 = new ThreadSort(items, mid, items.length);
+        ThreadSort t3 = new ThreadSort(items, 0, mid);
+        ThreadSort t4 = new ThreadSort(items, mid, items.length);
+
+        long startTime = System.nanoTime();
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+
+        MergeSort m1 = new MergeSort(t1.gettItems(), t2.gettItems());
+        m1.start();
+        m1.join();
+        MergeSort m2 = new MergeSort(t3.gettItems(), t4.gettItems());
+        m2.start();
+        m2.join();
+
+        MergeSort m3 = new MergeSort(m1.getSortedItems(), m2.getSortedItems());
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime)/1000000;
+
+        for(Item i : m2.getSortedItems()){
+            System.out.println(i);
+        }
+        System.out.println("Quad sort took: " + duration);
     }
 
     public static void DualSort(Item[] items) throws InterruptedException {
