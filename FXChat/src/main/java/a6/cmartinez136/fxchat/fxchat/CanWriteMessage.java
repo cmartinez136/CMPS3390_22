0a470ad22fcec9bd9a35876b0aa27675340d179f
Carolina Martinez <cmarinez136@csub.edu>
2022-03-18 12:27:56 -0700

    a6 done

---
+++ FXChat/src/main/java/a6/cmartinez136/fxchat/fxchat/CanWriteMessage.java
@@ -0,0 +1,6 @@
+package a6.cmartinez136.fxchat.fxchat;
+
+public interface CanWriteMessage {
+    public void writeMessage(String message);
+}
+
---
+++ FXChat/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module a6.cmartinez136.fxchat.fxchat {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens a6.cmartinez136.fxchat.fxchat to javafx.fxml;
+    exports a6.cmartinez136.fxchat.fxchat;
+}
